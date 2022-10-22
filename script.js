eb28bcf239ef85de34da15cb6d4407a751681ace
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-22 16:40:31 -0700

    finished add item

--- script.js
+++ script.js
@@ -100,6 +100,31 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Add to column list, reset textbox
+
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = "hidden";
+  saveItemBtns[column].style.display = "flex";
+  addItemContainers[column].style.display = "flex";
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = "visible";
+  saveItemBtns[column].style.display = "none";
+  addItemContainers[column].style.display = "none";
+  addToColumn(column);
+}
+
 // Allow arrays to reflect drag and drop items
 function rebuildArrays() {
   backlogListArray = [];
